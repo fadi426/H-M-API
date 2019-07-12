3479ea0201c4ad696427d5fe82f906f5cf562f5a
fadi426 <fadiqadadi@hotmail.com>
2019-07-12 13:46:24 +0200

    Added case to controller

--- TheStingAPI/src/controller/APIController.php
+++ TheStingAPI/src/controller/APIController.php
@@ -3,21 +3,28 @@ header("Content-Type:application/json");
 require "../service/ProductService.php";
 
 $fullPath = $_SERVER['REQUEST_URI'];
+$request_method=$_SERVER["REQUEST_METHOD"];
 $endpoint = substr($fullPath, strpos($fullPath, "?") + 1);
-if ($endpoint == "products")
-    response(200, "Products Found", getProducts());
-else if(!empty($_GET['id']))
+
+switch($request_method)
 {
-    $id=$_GET['id'];
-    $product = getProduct($id);
-    if(empty($product))
-        response(200,"Product Not Found",NULL);
-    else
-        response(200,"Product Found",$product);
-}
-else
-{
-    response(400,"Invalid Request",NULL);
+    case 'GET':
+        if ($endpoint == "products")
+            response(200, "Products Found", getProducts());
+        else if(!empty($_GET["id"]))
+        {
+            $id=$_GET['id'];
+            $product = getProduct($id);
+            if(empty($product))
+                response(200,"Product Not Found",NULL);
+            else
+                response(200,"Product Found",$product);
+        }
+        break;
+    default:
+        // Invalid Request Method
+        response(400,"Invalid Request",NULL);
+        break;
 }
 
 function response($status,$status_message,$data)
